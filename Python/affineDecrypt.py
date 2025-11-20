a656490a068133b6b8ed87b5751d63a3807329eb
Furby566 <john.mccraw@mines.sdsmt.edu>
2025-11-20 16:52:18 -0700

    added a loop to continue

--- Python/affineDecrypt.py
+++ Python/affineDecrypt.py
@@ -1,20 +1,29 @@
-string = input("What is your encrypted string? ") #takes in message
-string = string.lower()
+
+
 n = 0;
 i = 0;
-coefOne = int(input("State your first coefficent: ")) 
-coefTwo = int(input("Now your second: "))
-for i in range(1, 26):
-    if(coefOne*i)%26==1:
-        coefOne = i;
-        break;
-for n in range(len(string)):
-    if(string[n].isalpha() == False ):
-        ans = string[n]
+decision = "yes" 
+while decision == "yes":
+    string = input("What is your encrypted string? ") #takes in message
+    string = string.lower()#avoids pesky big letters
+
+    coefOne = int(input("State your first coefficent: ")) 
+    coefTwo = int(input("Now your second: "))
+    for i in range(1, 26):
+        if(coefOne*i)%26==1:
+            coefOne = i;
+            break;
+    for n in range(len(string)):
+        if(string[n].isalpha() == False ):
+            ans = string[n]
         
-    else:
-        curr = ord(string[n]) - ord('a')#brings it down to original num val
-        curr = (coefOne*(curr-coefTwo))%26 #uses inverse of affine ciper to decrypt
-        ans = curr +ord('a') #decrypts then converts to a new letter
-        ans = chr(ans)
-    print(ans, end="")
+        else:
+            curr = ord(string[n]) - ord('a')#brings it down to original num val
+            curr = (coefOne*(curr-coefTwo))%26 #uses inverse of affine ciper to decrypt
+            ans = curr +ord('a') #decrypts then converts to a new letter
+            ans = chr(ans)
+        
+        print(ans, end="")
+    decision = input("Would you like to continue: (yes/no) ")
+
+exit()
